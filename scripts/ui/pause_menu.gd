ef79e2f13b946560b5a060f1c77c7c062b7a5668
Lukas Vasiliauskas <lukas.vasi@gmail.com>
2024-12-05 19:55:04 +0200

    Added a basic text tutorial to the pause menu

--- scripts/ui/pause_menu.gd
+++ scripts/ui/pause_menu.gd
@@ -7,6 +7,8 @@ extends Control
 
 @onready var _statistics_ui : PanelContainer = get_node("StatisticsUI")
 
+@onready var _tutorial_ui : PanelContainer =get_node("TutorialUI")
+
 ## The player's camera. Only retrieves and works with the camera in main.
 @onready var player_camera: XRCamera3D = get_tree().get_first_node_in_group("player").get_node("XRCamera3D")
 
@@ -19,12 +21,17 @@ extends Control
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	# Hide all the UIs
-	_pause_menu_ui.visible = false
-	_settings_ui.visible = false
-	_statistics_ui.visible = false
+	_hide_all()
 	
 	# Connect to pause manager
 	PauseManager.pause_state_changed.connect(_on_pause_state_changed)
+
+
+func _hide_all() -> void:
+	_pause_menu_ui.visible = false
+	_settings_ui.visible = false
+	_statistics_ui.visible = false
+	_tutorial_ui.visible = false
 
 
 ## Method that handles the pause state change singal from PauseManager
@@ -32,17 +39,14 @@ func _on_pause_state_changed(paused : bool) -> void:
 	if paused:
 		_pause_menu_ui.visible = true
 	else:
-		_pause_menu_ui.visible = false
-		_settings_ui.visible = false
-		_statistics_ui.visible = false
+		_hide_all()
 
 func _on_resume_button_pressed() -> void:
 	PauseManager.set_pause(false)
 
 
 func _on_settings_button_pressed() -> void:
-	_pause_menu_ui.visible = false
-	_statistics_ui.visible = false
+	_hide_all()
 	_settings_ui.visible = true
 
 
@@ -56,8 +60,7 @@ func _on_exit_game_button_pressed() -> void:
 
 
 func _on_back_to_menu_button_pressed() -> void:
-	_settings_ui.visible = false
-	_statistics_ui.visible = false
+	_hide_all()
 	_pause_menu_ui.visible = true
 
 
@@ -66,6 +69,11 @@ func _player_height_changed(_new_height: float) -> void:
 
 
 func _on_statistics_button_pressed() -> void:
-	_pause_menu_ui.visible = false
-	_settings_ui.visible = false
+	_hide_all()
 	_statistics_ui.visible = true
+
+
+func _on_tutorial_button_pressed() -> void:
+	_hide_all()
+	_tutorial_ui.visible = true
+	
